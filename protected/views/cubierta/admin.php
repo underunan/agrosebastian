7cd8bcb35cf24e632c25c6cf325d42c3072d48a5
Horacio Javier Rodriguez <underunan@gmail.com>
2016-04-15 14:01:16 -0600

    Actualizando archivos

--- protected/views/cubierta/admin.php
+++ protected/views/cubierta/admin.php
@@ -26,21 +26,49 @@ $('.search-form form').submit(function(){
 ");
 ?>
 
-<h1>Manage Cubiertas</h1>
+<h1 class="text-center text-info">Administración de vacas preñadas</h1>
 
-<p>
-You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
-</p>
-
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
+<?php //echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
 <div class="search-form" style="display:none">
-<?php $this->renderPartial('_search',array(
+<?php/* $this->renderPartial('_search',array(
 	'model'=>$model,
-)); ?>
+)); ?> */
 </div><!-- search-form -->
 
-<?php $this->widget('zii.widgets.grid.CGridView', array(
+<?php 
+
+$this->widget(
+    'booster.widgets.TbGridView',
+    array(
+    	'columns'=>array(
+    		'idcubiertas',
+			'idganado',
+			array(
+				'header'=>'Nombre',
+				'name'=>'idganado',
+				'value'=>'$data->idganado0->nombre',
+				),
+			'fecha_cubierta',
+			'observacion',
+			array(
+				'header'=>'Controles',
+				'class'=>'booster.widgets.TbButtonColumn',
+				'htmlOptions' => array('nowrap'=>'nowrap'),
+				),
+		),
+		'id'=>'parto-grid',
+		'dataProvider'=>$model->search(),
+		'filter'=>$model,
+        'type' => 'striped condensed',
+    )
+);
+
+?>
+
+
+<?php 
+/*
+$this->widget('zii.widgets.grid.CGridView', array(
 	'id'=>'cubierta-grid',
 	'dataProvider'=>$model->search(),
 	'filter'=>$model,
@@ -53,4 +81,6 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 			'class'=>'CButtonColumn',
 		),
 	),
-)); ?>
+)); 
+*/
+?>
